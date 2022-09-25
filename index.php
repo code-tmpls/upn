e27d09c2b6c05bc8a29137734abaad43ad8a8f16
AdithyaKankipati <adithyakankipati28@gmail.com>
2022-09-25 19:30:13 +0530

    changed html.php to index.php

--- index.php
+++ index.php
@@ -13,11 +13,17 @@ $PROJECT_IMG_PATH = 'http://localhost/np/locumme_data/images/';
   <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
   <script src="js/html.js"></script>
 <style>
+h3 { color:#d0254e;padding-bottom:15px;border-bottom:1px solid #d0254e; }
+.bg-box { background-color:#c1e2ff;border-color:#024279; }
+.subheading { margin-top:15px; margin-bottom:15px; }
 a { color: #024279; }
 pre { background:none;border:0px; }
 body { font-size:12px;padding:15px; }
 .hide-block { display:none; }
 .output-bg { background-color:#fefefe; }
+.mtop5p { margin-top:5px; }
+.mtop15p { margin-top:15px; }
+.mtop25p { margin-top:25px; }
 .mbot5p { margin-bottom:5px; }
 img { width:300px;height:100%;padding:5px; }
 .pad5p { padding:5px; }
--- templates/indian-polity-01/01-background/00_basic_01.php
+++ templates/indian-polity-01/01-background/00_basic_01.php
@@ -1,1 +1,209 @@
-<div>Column-01</div>
+<div class="mtop25p">
+The Constitution of India was drawn up by a Constituent Assembly. The Assembly met for the first time 
+on December 9, 1946. It was not a truly representative body as its members were indirectly elected by 
+those who were themselves elected on a narrow franchise. The Assembly constituted a Drafting Committee, 
+under the chairmanship of Dr.B.R.Ambedkar, to frame a constitution for India. Indian Constitution was 
+adopted on November 26, 1949 and it came into effect on January 26, 1950.  It is the longest written 
+Constitution in the world containing 395 Articles, 22 Parts and 12 Schedules. During 60 years of its 
+existence, Indian Constitution has undergone several amendments and demand to review it completely has 
+also been raised. But, inspite of all these changes, the "basic structure" of constitution remain 
+intact.<br>
+
+
+<h3>Development of the Indian Constitution</h3><br>
+British rule in India ended on 15th August 1947 and India emerged as an independent and sovereign 
+republic. Certain features of Indian Polity or Constitution can be understood better.<br>
+<div class="list-group">
+<div class="list-group-item bg-box mtop15p">
+<strong>Acts that faciliated constitution development during the british rule</strong> 
+<ul>
+<li><i>Regulating Act, 1773</i></li>
+<li><i>Amending Act, 1781</i></li>
+<li><i>Pitt's India Act, 1784</i></li>
+<li><i>Act of 1786</i></li>
+<li><i>Charter Act, 1793</i></li>
+<li><i>Charter Act, 1813</i></li>
+<li><i>Charter Act, 1833</i></li>
+<li><i>Charter Act, 1853</i></li>
+<li><i>Government of India Act, 1858</i></li>
+<li><i>Indian Councils Act, 1861</i></li>
+<li><i>Indian Councils Act, 1892</i></li>
+<li><i>Indian Councils Act, 1909</i></li>
+<li><i>Government of India Act, 1919</i></li>
+<li><i>Government of India Act, 1935</i></li>
+<li><i>Indian Independence Act, 1947</i></li>
+</ul>
+</div>
+</div>
+with a brief review of the constitutional set up in the preceding periods. As modern political 
+institutions originated and developed in India mainly during the British rule, the origin and 
+growth of the Indian Constitution has its roots in the British period of Indian history. 
+The British came to India in the 17th century as traders. From 1773 onwards, various Acts were 
+passed by the British Government for the governance of India. 
+
+<br/>
+<br/>
+
+<h3>Sources of Indian Constitution</h3><br/>
+The sources of Indian Constitution include the imaginative aspirations of the nationalist leaders, the 
+actual working of the Government of India Act, 1935, and the experience gained from the actual working 
+of some of the Constitutions of important countries of the world. Moreover, its sources include not only 
+the sources upon which the founding fathers of our Constitution drew but also the developmental sources 
+such as the judicial decisions, constitutional amendments, constitutional practices and so on.<br/><br/>
+The sources of the Indian Constitution can thus be divided into the following two categories:
+<br/>
+
+<div class="list-group">
+<div class="list-group-item bg-box mtop15p">
+
+<ol>
+    <li><b>Seminal Sources</b>
+        <ul>
+            <li><i>Constituent Assembly Debates</i></li>
+            <li><i>Reports of Committees of the Constituent Assembly</i></li>
+            <li><i>Nehru Report</i></li>
+            <li><i>Lahore Session of the Indian National Congress</i></li>
+            <li><i>Objectives Resolution</i></li>
+            <li><i>Government of India Act, 1935</i></li>
+            <li><i>Impact of various Constitutions</i></li>
+        </ul>
+    </li>
+    <li><b>Development Sources</b>
+        <ul>
+            <li><i>Amendments of the Constitution</i></li>
+            <li><i>Judicial Decisions</i></li>
+            <li><i>Parliamentary Statutes</i></li>
+            <li><i>Commentaries of Constitutional Experts</i></li>
+            <li><i>Rules, Regulatios, Ordinances, etc</i></li>
+            <li><i>Constitutional Practices</i></li>
+        </ul>
+    </li>
+</ol>
+
+</div>
+</div>
+<ol>
+<li><div class="subheading"><strong>Seminal Sources</strong></div>
+<ul>
+
+<li>
+<div class="subheading"><strong><i>Constituent Assembly Debates:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Reports of Committees of the Constituent Assembly:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Nehru Report:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Lahore Session of the Indian National Congress:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Objectives Resolution:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Government of India Act, 1935:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Impact of various Constitutions:</i></strong></div>
+<div></div>
+</li>
+
+</ul>
+</li>
+<li><div class="subheading"><strong>Development Sources</strong></div>
+<ul>
+
+<li>
+<div class="subheading"><strong><i>Amendments of the Constitution:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Judicial Decisions:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Parliamentary Statutes:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Commentaries of Constitutional Experts:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Rules, Regulatios, Ordinances, etc:</i></strong></div>
+<div></div>
+</li>
+
+<li>
+<div class="subheading"><strong><i>Constitutional Practices:</i></strong></div>
+<div></div>
+</li>
+
+</ol>
+
+<!-- -->
+<ol>
+<li><strong>Seminal Sources</strong></li>
+
+<b><i>Constituent Assembly Debates:</i></b> Constituent Assembly was constituted under the Cabinet Mission plan to frame the Indian Constitution. Its members included distinguished lawyers, intellectuals and patriots who took two years, eleven months and eighteen days to prepare the constitution. During the course of this period, debates on all the aspects of the Constitution were held in a free and fair manner. These debates produced an intelligent opinion in the light of which every word of the Constitution was screened carefully and intelligently.
+<br>
+<strong>Reports of Committees of the Constituent Assembly:</strong>
+The Constituent Assembly appointed various types of committees to make reports on different aspects. Some of the most important committees included Union Powers Committees, Union Constitution Committee, Provincial Constitution Committee, etc. Advisory Committed on the rights of citizens, minorities and tribal and excluded areas and hoc Committee regarding the Supreme Court Committee on financial relations between the Union and States. The reports of these committees were thoroughly discussed in the Drafting Committee. It was on the basis of the reports made by such committees that the draft of the Constitution was prepared.
+<br>
+<strong>Nehru Report:</strong>
+The british Government had announced the formation of the Simon Commision for making a report on the working of the Government of India Act, 1919 and suggest constitutional measures required for the efficient administration of Indian. The Congress decided to boycott this Commision because no Indian was taken on it. Instead it announced the formation of a committee under the leadership of Motilal Nehru  to make recommendations about the desired constitutional set up for India. The Committee made certain recommendations which are known as the Nehru Report. The main Clauses of this Report were : 
+<li>grant of Empire;</li>
+<li>Creation of a federal structure for India </li> 
+<li>bicameralism at the Centre;</li>   
+<li>Parliamentary and responsible Government in Provinces;</li>  
+<li>guarantee of Fundamental Right;</li>
+<li>establishment of Supreme Court as the final court of appeal.</li>
+
+<br>
+<strong>Lahore Session of the Congress:</strong>  The Congress at its Lahore session held in 1929 resolved to make India a Republic.
+<strong>Objectives Resolution:</strong>  Jawharlal Lal Nehru moved in the Constituent Assembly the Objectives Resolution embodying the aspirations of nationalist India. The Objectives Resolution clearly spelled out making India a sovereign republic where the ultimate supreme power should be vested with the people. It stated that the people would get social, economic and political justice, liberties of all types and equality. Upliftment of the backward people and areas would be ensured and the structure of the country would be federal.
+<br>
+<strong>Government of India Act 1935:</strong>
+ At the time the Constitution for free India was being framed, India was governed by the Government of India Act, 1935. The fathers of the Indian Constitution drew heavily on the experience and the provisions of this Act. According to Jennings - "The Constitution derives directly from the Government of India Act, 1935 from which, in fact, many of its provision are copied textually."
+<br>
+ <strong>Impact of Various Constitutions:</strong>
+ The founding fathers of the Indian Constitution were wise enough to borrow from the experience gained in the working of various other Constitutions. It is on this account that the Constitution of India is regarded as a bag of borrowing from the various working Constitutions 
+</ol>
+<!-- -->
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

61531fe0740146ae16c12ab4ecdb74a2a288e8f7
AdithyaKankipati <adithyakankipati28@gmail.com>
2022-09-30 22:45:47 +0530

    Updated data

---
+++ templates/indian-polity-02/01-judiciary/00_basic_01.php
@@ -0,0 +1,188 @@
+<div class="mtop25">
+
+<h3>Judiciary</h3>
+
+<div class="subheading"><strong><i>The Supreme Court</i></strong></div>
+The Constitution of India has the following
+provisions regarding the Supreme Court of India:
+
+<div class="subheading"><strong><i>Article 124:</i></strong></div>
+<b>Establishment and constitution of Supreme
+Court</b>
+<ol>
+    <ul>
+        <li>There shall be a Supreme Court of India
+consisting of a Chief Justice of India and,
+until Parliament by law prescribes a larger
+number, of not more than seven.</li>
+        <li>Every Judge of the Supreme Court shall be
+appointed by the President by warrant
+under his hand and seal after consultation
+with such of the Judges of the Supreme
+Court and of the High Courts in the States
+as the President may deem necessary for
+the purpose and shall hold office until he
+attains the age of sixty-five years:
+Provided that in the case of appointment
+of a Judge other than the Chief Justice, the
+Chief Justice of India shall always be
+consulted. Provided further that
+<ol>
+    <ul>
+        <li>a Judge may, by writing under his hand
+addressed to the President, resign his
+office;</li>
+<li>a Judge may be removed from his office
+in the manner provided in clause (4).
+The age of a Judge of the Supreme Court shall
+be determined by such authority and in such
+manner as Parliament may by law provide.</li>
+    </ul>
+</ol>
+</li>
+        <li>A person shall not be qualified for appointment as a Judge of the Supreme
+Court unless he is a citizen of India and-
+<ol>
+    <ul>
+        <li>has been for at least five years a Judge
+of a High Court or of two or more
+such Courts in succession; or</li>
+<li>has been for at least ten years an
+advocate of a High Court or of two or
+more such Courts in succession; or</li>
+<li>is, in the opinion of the President, a
+distinguished jurist.</li>
+    </ul>
+</ol> </li>
+<li>Parliament may by law regulate the procedure for the presentation of an address
+and for the investigation and proof of the
+misbehaviour or incapacity of a Judge
+under clause (4).</li>
+<li>Every person appointed to be a Judge of
+the Supreme Court shall, before he enters
+upon his office, make and subscribe before
+the President, or some person appointed in
+that behalf by him, an oath or affirmation
+according to the form set out for the
+purpose in the Third Schedule.</li>
+<li>No person who has held office as a Judge
+of the Supreme Court shall plead or act in
+any court or before any authority within
+the territory of India.</li>
+    </ul>
+</ol>
+
+<div class="subheading"><strong><i>Article 125:</i></strong></div>
+<b>Salaries, etc., of Judges.</b>
+<ol>
+    <ul>
+        <li>There shall be paid to the Judges of the
+Supreme Court such salaries as may be
+determined by Parliament by law and,
+until provision in that behalf is so made,
+such salaries as are specified in the Second
+Schedule.</li>
+        <li>Every Judge shall be entitled to such privileges and allowances and to such rights in
+respect of leave of absence and pension as
+may from time to time be determined by
+or under law made by Parliament and,
+until so determined, to such privileges,
+allowances and rights as are specified in
+the Second Schedule:</li>
+</ul>
+</ol>   
+Provided that neither the privileges nor the
+allowances of a Judge nor his rights in respect of
+leave of absence or pension shall be varied to his
+disadvantage after his appointment.
+   
+<div class="subheading"><strong><i>Article 126:</i></strong></div>
+
+<b>Appointment of acting Chief Justice.</b>
+When the office of Chief Justice of India is
+vacant or when the Chief Justice is, by reason of
+absence or otherwise, unable to perform the duties
+of his office, the duties of the office shall be
+performed by such one of the other Judges of the
+Court as the President may appoint for the
+purpose.
+
+<div class="subheading"><strong><i>Article 127:</i></strong></div>
+<b>Appointment of ad hoc Judges.</b>
+If at any time there should not be a
+quorum of the Judges of the Supreme
+Court available to hold or continue any
+session of the Court, the Chief Justice of
+India may, with the previous consent of
+the President and after consultation with
+the Chief Justice of the High Court concerned, request in writing the attendance
+at the sittings of the Court, as an ad hoc
+Judge, for such period as may be necessary, of a Judge of a High Court duly
+qualified for appointment as a Judge of the
+Supreme Court to be designated by the
+Chief Justice of India.
+(2) It shall be the duty of the Judge who has
+been so designated, in priority to other
+duties of his office, to attend the sittings of
+the Supreme Court at the time and for the
+period for which his attendance is required, and while so attending he shall
+have all the jurisdiction, powers and privileges, and shall discharge the duties, of a
+Judge of the Supreme Court.
+Article 128:
+Attendance of retired Judges at sittings of the
+Supreme Court.
+Notwithstanding anything in this Chapter,
+the Chief Justice of India may at any time, with
+the previous consent of the President, request any
+person who has held the office of a Judge of the
+Supreme Court or of the Federal Court 2[or who
+has held the office of a Judge of a High Court and
+is duly qualified for appointment as a Judge of the
+Supreme Court] to sit and act as a Judge of the
+Supreme Court, and every such person so requested
+shall, while so sitting and acting, be entitled to
+such allowances as the President may by order
+determine and have all the jurisdiction, powers
+and privileges of, but shall not otherwise be
+deemed to be, a Judge of that Court:
+Provided that nothing in this article shall be
+deemed to require any such person as aforesaid
+to sit and act as a Judge of that Court unless he
+consents so to do.
+Article 129:
+Supreme Court to be a court of record.
+The Supreme Court shall be a court of record
+and shall have all the powers of such a court
+including the power to punish for contempt of
+itself.
+Article 130:
+Seat of Supreme Court.
+The Supreme Court shall sit in Delhi or in such
+other place or places, as the Chief Justice of India
+may, with the approval of the President, from time
+to time, appoint.
+Article 131:
+Original jurisdiction of the Supreme Court.
+Subject to the provisions of this Constitution,
+the Supreme Court shall, to the exclusion of any
+other court, have original jurisdiction in any
+dispute-
+(a) between the Government of India and one
+or more States; or
+(b) between the Government of India and any
+State or States on one side and one or more
+other States on the other; or
+(c) between two or more States, if and in so far
+as the dispute involves any question (whether
+of law or fact) on which the existence or
+extent of a legal right depends:
+Provided that the said jurisdiction shall not
+extend to a dispute arising out of any treaty,
+agreement, covenant, engagement, sanad or other
+similar instrument which, having been entered
+into or executed before the commencement of this
+Constitution, continues in operation after such
+commencement, or which provides that the said
+jurisdiction shall not extend to such a dispute
+
+</div>
